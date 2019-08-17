0790704bbd38bfe788cc392acb9ff4c24db98936
Darren Kellenschwiler <darrkellen@gmail.com>
2019-08-17 13:00:38 +0100

    added test folder

---
+++ tests/mongotest.js
@@ -0,0 +1,13 @@
+// Connect using a MongoClient instance
+const MongoClient = require('mongodb').MongoClient;
+const test = require('assert');
+// Connection url
+const url = 'mongodb://localhost:27017';
+// Database Name
+const dbName = 'test';
+// Connect using MongoClient
+const mongoClient = new MongoClient(url);
+mongoClient.connect(function(err, client) {
+  const db = client.db(dbName);
+  client.close();
+});
