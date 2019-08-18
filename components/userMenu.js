025b632af8e29a38446b3ecca8a149842d0902ec
Siko91 <alexander.n.dinkov@gmail.com>
2019-08-18 17:31:56 +0300

    logout funvtionality & access to user settings

--- components/userMenu.js
+++ components/userMenu.js
@@ -1,54 +1,81 @@
-import React, { Component } from 'react'
-import { Nav, NavItem, NavLink } from 'reactstrap'
-import { NextAuth } from 'next-auth/client'
-import Router from 'next/router'
-import Cookies from 'universal-cookie'
+import React, { Component } from "react";
+import { Nav, Button, NavItem, NavLink } from "reactstrap";
+import { NextAuth } from "next-auth/client";
+import Router from "next/router";
+import Cookies from "universal-cookie";
+import { FormPageFunctions } from "../components/formPageFunctions";
 
 class UserMenu extends Component {
-  constructor (props) {
-    super(props)
-    this.handleSignoutSubmit = this.handleSignoutSubmit.bind(this)
+  static async getInitialProps({ req }) {
+    let props = await super.getInitialProps({ req });
+    return FormPageFunctions.getInitialProps(this, req, props);
+  }
+  constructor(props) {
+    super(props);
+    FormPageFunctions.construct(this, props);
+    this.handleSignoutSubmit = this.handleSignoutSubmit.bind(this);
+  }
+  async componentDidMount() {
+    FormPageFunctions.componentDidMount(this);
   }
 
-  async handleSignoutSubmit (event) {
-    event.preventDefault()
+  async handleSignoutSubmit(event) {
+    event.preventDefault();
 
     // Save current URL so user is redirected back here after signing out
-    const cookies = new Cookies()
-    cookies.set('redirect_url', window.location.pathname, { path: '/' })
+    const cookies = new Cookies();
+    cookies.set("redirect_url", window.location.pathname, { path: "/" });
 
-    await NextAuth.signout()
-    Router.push('/')
+    await NextAuth.signout();
+    Router.push("/");
   }
 
-  render () {
+  render() {
     if (this.props.session && this.props.session.user) {
       // If signed in display user dropdown menu
-      const session = this.props.session
+      const session = this.props.session;
       return (
-        <Nav id='topNavigation' navbar>
-          <NavLink href='/ask' className='topNavItem btn btn-outline-primary'>
+        <Nav id="topNavigation" navbar>
+          <NavLink href="/ask" className="topNavItem btn btn-outline-primary">
             Ask
           </NavLink>
-          <NavLink href='/questions' className='topNavItem btn btn-outline-primary'>
+          <NavLink
+            href="/questions"
+            className="topNavItem btn btn-outline-primary"
+          >
             Questions
           </NavLink>
-          <NavLink href={"/profile?id=" + this.props.session.user.id} className='topNavItem btn btn-outline-primary'>
+          <NavLink
+            href={"/profile?id=" + this.props.session.user.id}
+            className="topNavItem btn btn-outline-primary"
+          >
             Profile
           </NavLink>
+          <NavLink
+            href="/account/logout"
+            className="topNavItem topNavItem-logout btn btn-outline-primary"
+          >
+            Logout
+          </NavLink>
         </Nav>
-      )
+      );
     } else {
       // If not signed in, display sign in button
       return (
-        <Nav id='topNavigation' navbar>
-          <NavLink href='/ask' className='topNavItem btn btn-outline-primary'>
+        <Nav id="topNavigation" navbar>
+          <NavLink href="/ask" className="topNavItem btn btn-outline-primary">
             Ask
           </NavLink>
-          <NavLink href='/questions' className='topNavItem btn btn-outline-primary'>
+          <NavLink
+            href="/questions"
+            className="topNavItem btn btn-outline-primary"
+          >
             Questions
           </NavLink>
-          <NavLink href='/auth?redirect=/' className='topNavItem btn btn-outline-primary'>
+          <NavLink
+            href="/auth?redirect=/"
+            className="topNavItem btn btn-outline-primary"
+          >
             {/**
              * @TODO Add support for passing current URL path as redirect URL
              * so that users without JavaScript are also redirected to the page
@@ -57,9 +84,9 @@ class UserMenu extends Component {
             <span onClick={this.props.toggleModal}>Sign up / Sign in</span>
           </NavLink>
         </Nav>
-      )
+      );
     }
   }
 }
 
-export default UserMenu
+export default UserMenu;
--- pages/profile.js
+++ pages/profile.js
@@ -1,45 +1,58 @@
 // Profile Page
-import Link from 'next/link'
-import React from 'react'
-import { Container, Row, Col, Button, Jumbotron, ListGroup, ListGroupItem } from 'reactstrap'
-import Page from '../components/page'
-import Layout from '../components/layout'
+import Link from "next/link";
+import React from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Jumbotron,
+  ListGroup,
+  ListGroupItem
+} from "reactstrap";
+import Page from "../components/page";
+import Layout from "../components/layout";
 
 class Profile extends Page {
-  render () {
+  render() {
     return (
       <Layout {...this.props} navmenu={false} container={false}>
         <Container>
           {this.getGreeting()}
-          <ListGroup id="profileQuestionsOverview"/>
+          <ListGroup id="profileQuestionsOverview" />
           <ListGroup id="profileAnswersOverview" />
         </Container>
       </Layout>
-    )
+    );
   }
 
   getGreeting = () => {
-    if(this.props.session && this.props.session.user){
+    if (this.props.session && this.props.session.user) {
       return (
-        <div id="profileGreeting">Hello {this.props.session.user.name}</div>
-      )
-    }
-    else {
+        <div id="profileGreeting">
+          <div> Hello {this.props.session.user.name}</div>
+          <Link href="/account">
+            <Button className="btn btn-outline-primary">
+              Go to Settings...
+            </Button>
+          </Link>
+        </div>
+      );
+    } else {
       return null;
     }
-  }
-
+  };
 
   componentDidMount() {
-    fetch("/questions/all", { headers: {q:null, l:25} })
+    fetch("/questions/all", { headers: { q: null, l: 25 } })
       .then(res => res.json())
       .then(data => this.displayQuestions(data))
       .catch(err => console.log("Error getting Questions", err));
   }
 
-  displayQuestions = () => {
-    console.log(data)
-  }
+  displayQuestions = data => {
+    console.log(data);
+  };
 }
 
-export default Profile
+export default Profile;
