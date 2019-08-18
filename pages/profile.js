e747dee3301584a3b6a6836245577c0ff53dddb7
Siko91 <alexander.n.dinkov@gmail.com>
2019-08-18 19:57:27 +0300

    profile - show answers

--- pages/profile.js
+++ pages/profile.js
@@ -44,18 +44,25 @@ class Profile extends Page {
   };
 
   componentDidMount = () => {
-    var userID = this.props.session.user.id
+    var userID = this.props.session.user.id;
     fetch("/questions/user/" + userID, { headers: { l: 10 } })
       .then(res => res.json())
       .then(data => this.displayQuestions(data))
       .catch(err => console.log("Error getting Questions", err));
-  }
+
+    fetch("/questions/answered/" + userID, { headers: { l: 10 } })
+      .then(res => res.json())
+      .then(data => this.displayAnswers(data))
+      .catch(err => console.log("Error getting Questions", err));
+  };
 
   displayQuestions = data => {
     console.log("Questions", data);
     var ul = window.profileQuestionsOverview;
 
-    $("<h2>").append("Recent Questions").appendTo(ul);
+    $("<h2>")
+      .append("Recent Questions")
+      .appendTo(ul);
 
     for (let i = 0; i < data.length; i++) {
       const q = data[i];
@@ -92,7 +99,87 @@ class Profile extends Page {
         )
         .appendTo(ul);
     }
-  }
+  };
+
+  displayAnswers = data => {
+    console.log("Answers", data);
+    var ul = window.profileAnswersOverview;
+
+    $("<h2>")
+      .append("Recent Answers")
+      .appendTo(ul);
+
+    var answers = data
+      .map(q =>
+        q.answers.map(a => {
+          a.qId = q._id;
+          a.qTitle = q.title;
+          return a;
+        })
+      )
+      .reduce((a, b) => a.concat(b), [])
+      .filter(a => a.userId === this.props.session.user.id);
+
+    for (let i = 0; i < answers.length; i++) {
+      const a = answers[i];
+
+      var answerResponse = "Unchecked";
+      var answerResponseClass = "answer-unchecked";
+      if (a.response && a.response.approval == false) {
+        answerResponse = "Bad Answer";
+        answerResponseClass = "answer-bad";
+      }
+      if (a.response && a.response.approval) {
+        answerResponse =
+          "<a href='https://whatsonchain.com/tx/" +
+          a.response.payment.txId +
+          "'><button class='btn btn-outline-success'>Received " +
+          a.response.payment.amount +
+          " Sat</button></a>";
+        answerResponseClass = "answer-good";
+      }
+
+      $("<a>")
+        .attr("href", "/question?id=" + a.qId)
+        .addClass("answerListItem")
+        .append(
+          $("<div>")
+            .addClass("row")
+            .append(
+              $("<div>")
+                .addClass("col col-8")
+                .append(
+                  $("<div>")
+                  .append("Q: ").append(
+                    $("<span>")
+                      .addClass("answer-questionTitle")
+                      .append(a.qTitle)
+                  )
+                )
+                .append(
+                  $("<div>")
+                    .addClass("answerText")
+                    .append("A: ")
+                    .append($("<span>").append(a.text))
+                )
+            )
+            .append(
+              $("<span>")
+                .append(answerResponse)
+                .addClass("col-2")
+                .addClass("answerResponse")
+                .addClass(answerResponseClass)
+            )
+            .append(
+              $("<span>")
+                .addClass("questionPromised")
+                .addClass("col-2")
+                .append(a.ontime ? "Answered on time" : "Answer was late")
+            )
+        )
+        .appendTo(ul);
+    }
+  };
 }
 
 export default Profile;
