fbfb4ca24bbecd49a833c46b5ecaaf312cedfccf
Siko91 <alexander.n.dinkov@gmail.com>
2019-08-18 15:35:22 +0300

    question page - get request works

--- pages/question.js
+++ pages/question.js
@@ -1,32 +1,41 @@
-import Link from 'next/link'
-import React from 'react'
-import { Container, Row, Col, Button, Jumbotron, ListGroup, ListGroupItem } from 'reactstrap'
-import Page from '../components/page'
-import Layout from '../components/layout'
-import QuestionFull from '../components/questionFull'
-import fetch from 'isomorphic-fetch'
+import Link from "next/link";
+import React from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Jumbotron,
+  ListGroup,
+  ListGroupItem
+} from "reactstrap";
+import Page from "../components/page";
+import Layout from "../components/layout";
+import QuestionFull from "../components/questionFull";
+import fetch from "isomorphic-fetch";
 
 class Question extends Page {
-  componentDidMount () {
-    var urlParams = new URLSearchParams(window.location.search)
-    var questionId = urlParams.get('id')
+  componentDidMount() {
+    var urlParams = new URLSearchParams(window.location.search);
+    var questionId = urlParams.get("id");
+    console.log(questionId);
     fetch(`/questions/question/${questionId}`)
       .then(res => res.json())
       .then(data => this.displayQuestion(data))
-      .catch(err => console.log("Error getting Question", err))
+      .catch(err => console.log("Error getting Question", err));
   }
 
-  displayQuestion (data) {
-    console.log(data)
+  displayQuestion(data) {
+    console.log(data);
   }
 
-  render () {
+  render() {
     return (
       <Layout {...this.props} navmenu={false} container={false}>
         <QuestionFull />
       </Layout>
-    )
+    );
   }
 }
 
-export default Question
+export default Question;
